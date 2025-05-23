9fbeb9cf278c3d5710afa2e109ebd059d5c2b7d7
slayervictor <victor.r2002@gmail.com>
2025-05-23 15:21:57 +0200

    solve suvat ex

--- Lectures/__init__.py
+++ Lectures/__init__.py
@@ -1,3 +1,4 @@
 from .Kinematics2D import Vector, VectorPair, evalf
+from .Kinematics1D import solve_suvat
 from . import dependencies
-__all__ = ["dependencies", "Vector", "VectorPair", "evalf"]
+__all__ = ["dependencies", "Vector", "VectorPair", "evalf", "solve_suvat"]

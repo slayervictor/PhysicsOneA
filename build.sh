ee9282eb8411bdf4c09ceba443e1e03abe135445
Raaaainbow <seba7204@gmail.com>
2025-05-24 11:56:16 +0200

    build.sh created

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+rm -rf build/ dist/ *.egg-info/
+python -m build
+ls -la dist/
+
+echo "To upload: twine upload dist/*"
+echo "TestPyPI: twine upload --repository testpypi dist/*"

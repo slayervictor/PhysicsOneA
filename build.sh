221372fc1d32647f9fb5b4a3b74d4bdfdfd562e0
Raaaainbow <seba7204@gmail.com>
2025-05-25 17:20:10 +0200

    version 0.1.1 release

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ rm -rf build/ dist/ *.egg-info/
 python -m build
 ls -la dist/
 
+pip install -e .
+
 echo "To upload: twine upload dist/*"
 echo "TestPyPI: twine upload --repository testpypi dist/*"
 echo "Now installing built package"
-pip install -e .

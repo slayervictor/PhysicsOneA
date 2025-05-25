0d2ca4e6ba65ab6134f4d1c9e179b20a0d680d5a
slayervictor <victor.r2002@gmail.com>
2025-05-25 11:49:10 +0200

    deeeep

--- src/PhysicsOneA/dependencies.py
+++ src/PhysicsOneA/dependencies.py
@@ -1,6 +1,5 @@
 from typing import Union
 import matplotlib.pyplot as plt
-from uncertainties import *
-#from uncertainties import ufloat, UFloat, nominal_value
-#from uncertainties.umath import sqrt as usqrt, sin as usin, cos as ucos, tan as utan, atan2 as uatan2
-#from uncertainties.unumpy import nominal_values, std_devs, sqrt, sum as usum
+from uncertainties import ufloat, UFloat, nominal_value
+from uncertainties.umath import sqrt as usqrt, sin as usin, cos as ucos, tan as utan, atan2 as uatan2
+from uncertainties.unumpy import nominal_values, std_devs, sqrt, sum as usum

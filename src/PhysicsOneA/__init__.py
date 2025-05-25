c10a161d95d4dae5c31cc3998172fb4b6d0409e5
slayervictor <victor.r2002@gmail.com>
2025-05-25 11:39:00 +0200

    helper shit

--- src/PhysicsOneA/__init__.py
+++ src/PhysicsOneA/__init__.py
@@ -1,5 +1,5 @@
 from .Kinematics2D import Vector, VectorPair, Projectile, CircularMotion, RelativeMotion
 from .Kinematics1D import solve_suvat
-from .helpers import speed_converter, radian_to_degree, gravity, degree_to_radian, calculate_from_angle, calculate_from_sides
+from .helpers import speed_converter, radian_to_degree, gravity, degree_to_radian, calculate_from_angle, calculate_from_sides, format_input
 from . import dependencies
-__all__ = ["dependencies", "Vector", "RelativeMotion", "calculate_from_angle", "calculate_from_sides", "VectorPair", "solve_suvat", "speed_converter", "Projectile", "radian_to_degree", "gravity", "degree_to_radian", "CircularMotion"]
+__all__ = ["dependencies", "Vector", "RelativeMotion", "calculate_from_angle", "calculate_from_sides", "VectorPair", "solve_suvat", "format_input", "speed_converter", "Projectile", "radian_to_degree", "gravity", "degree_to_radian", "CircularMotion"]
